564dd85e337a319ec632cfa26c90b802e6f4a769
Shubham <shubham.bhadauria.20091112@gmail.com>
2026-08-13 00:02:08 +0530

    feat: clean up receiver repo, remove android files, add detailed root readme

--- virtual_steer/app/src/main/java/com/example/virtual_steer/model/DiscoveredServer.kt
+++
@@ -1,8 +0,0 @@
-package com.example.virtual_steer.model
-
-data class DiscoveredServer(
-    val ip: String,
-    val port: Int,
-    val name: String,
-    val lastSeen: Long = System.currentTimeMillis()
-)
--- virtual_steer/app/src/main/java/com/example/virtual_steer/model/DiscoveryResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.example.virtual_steer.model
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class DiscoveryResponse(
-    val hostname: String,
-    val port: Int
-)
